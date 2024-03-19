e979e6efcc4d75f501d250ac1843e37693df810c
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-03-19 22:41:38 +0530

    added not implemented to problem controllers

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,15 +1,37 @@
-function addProblem(req, res) {}
-
-function getProblem(req, res) {}
-
-function getProblems(req, res) {}
-
-function deleteProblem(req, res) {}
-
-function updateProblem(req, res) {}
+const { StatusCodes } = require("http-status-codes");
 
 function pingProblemController(req, res) {
   return res.json({ message: "Problem controller is up" });
+}
+
+function addProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
+
+function getProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
+
+function getProblems(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
+
+function deleteProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
+}
+
+function updateProblem(req, res) {
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+  });
 }
 
 module.exports = {
