6c502635325dbb0378c2b45abd574328dd9db186
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-04-04 20:46:16 +0530

    Added get all problems api

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -15,6 +15,15 @@ class ProblemRepository {
       throw error;
     }
   }
+  async getAllProblems() {
+    try {
+      const problems = await Problem.find({});
+      return problems;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 
 module.exports = ProblemRepository;
--- src/services/problem.service.js
+++ src/services/problem.service.js
@@ -7,16 +7,14 @@ class ProblemService {
 
   async createProblem(problemData) {
     // 1. Sanitizue the markdown for description
-    try {
-      problemData.description = sanitizeMarkdownContent(
-        problemData.description
-      );
-      const problem = await this.problemRepository.createProblem(problemData);
-      return problem;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+    problemData.description = sanitizeMarkdownContent(problemData.description);
+    const problem = await this.problemRepository.createProblem(problemData);
+    return problem;
+  }
+
+  async getAllProblems() {
+    const problems = await this.problemRepository.getAllProblems();
+    return problems;
   }
 }
 
