dc1cc74605eef1ad245e5c09610c44a301ca20c4
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-04-04 21:26:04 +0530

    Added delete problem api

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -38,6 +38,19 @@ class ProblemRepository {
       throw error;
     }
   }
+
+  async deleteProblem(id) {
+    try {
+      const deletedProblem = await Problem.findByIdAndDelete(id);
+      if (!deletedProblem) {
+        throw new NotFound("problem", id);
+      }
+      return deletedProblem;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 
 module.exports = ProblemRepository;
